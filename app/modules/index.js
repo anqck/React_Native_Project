ecf20b02fe34d8f9f43139e3764327b10f97c753
anqck <quanchikhanhan@gmail.com>
2018-11-24 11:23:42 +0700

    - Update Redux, Store
    - Add Redux, Store, Recompose

---
+++ app/modules/index.js
@@ -0,0 +1,23 @@
+import AsyncStorage from 'redux-persist/es/storage';
+import { persistCombineReducers } from 'redux-persist';
+import app from './app';
+
+
+const config = {
+  key: 'root',
+  whitelist: [
+    'settings',
+    'accounts',
+    'categories',
+    'transactions',
+    'transfers',
+  ],
+  storage: AsyncStorage,
+};
+
+const appReducer = {
+  app,
+};
+
+
+ export default persistCombineReducers( appReducer);
