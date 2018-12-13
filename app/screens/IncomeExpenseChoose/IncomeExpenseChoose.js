fc57e94f38375f19058020a7a7147c06a53cb69b
anqck <quanchikhanhan@gmail.com>
2018-12-13 23:36:21 +0700

    - Update some stuff
    - Update some stuff

---
+++ app/screens/IncomeExpenseChoose/IncomeExpenseChoose.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+class Bookmarks extends Component {
+    render() {
+        return (
+            <SafeAreaView style={{
+                flex: 1,
+                alignItems: 'center',
+                justifyContent: 'center'
+            }}>
+                <Text>Bookmarks</Text>
+            </SafeAreaView>
+        );
+    }
+}
+
+export default Bookmarks;
