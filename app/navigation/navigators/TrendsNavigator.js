243220aa87bdefb7fc8c55486047a4a1630f75f0
anqck <quanchikhanhan@gmail.com>
2018-12-17 17:33:45 +0700

    - Update Routing for Trend

---
+++ app/navigation/navigators/TrendsNavigator.js
@@ -0,0 +1,30 @@
+import { TabNavigator,createBottomTabNavigator,createMaterialTopTabNavigator  } from 'react-navigation';
+
+import TrendsRoutes from '../routes/TrendsRoutes';
+
+const config = {
+//   animationEnabled: false,
+//   tabBarPosition: 'bottom',
+
+  tabBarOptions: { 
+  
+      showIcon: false ,
+      showLabel: true,
+      activeTintColor: '#171F33',
+      inactiveTintColor: '#586589',
+      style: {
+        //Styling of Tab
+        backgroundColor: '#48A2F8',
+      },
+      indicatorStyle: {
+        //Active Tab broder bottom color and width 
+        borderBottomColor: '#87B56A',
+        borderBottomWidth: 4,
+      },
+},
+
+
+   tabStyle: {}
+};
+
+export default createMaterialTopTabNavigator(TrendsRoutes,  config);
