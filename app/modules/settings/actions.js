f50cf28d86853743c8b8ac58d59fe392575a4d8c
anqck <quanchikhanhan@gmail.com>
2018-12-17 13:57:35 +0700

    - Add Reset date function

--- app/modules/settings/actions.js
+++ app/modules/settings/actions.js
@@ -1,7 +1,7 @@
 import { createActions } from 'redux-actions';
 import types from './types';
 
-export const { changeCurrency, signIn, generateMockData, resetData } = createActions(
+export const { signIn, generateMockData, resetData } = createActions(
   types.SIGN_IN,
   types.GENERATE_MOCK_DATA,
   types.RESET_DATA,
